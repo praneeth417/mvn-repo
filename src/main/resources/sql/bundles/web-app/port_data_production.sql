e8737fc374a96231b0a1de32e67fc4189042bae6
praneeth <praneeth417@gmail.com>
2022-08-30 21:58:10 +0530

    deleted

---
+++ src/main/resources/sql/bundles/web-app/port_data_production.sql
@@ -0,0 +1,5 @@
+INSERT INTO showletcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('web-app', '<?xml version="1.0" encoding="UTF-8"?>
+<properties>
+<property key="en">bpmweb</property>
+<property key="it">bpmweb</property>
+</properties>', NULL, NULL, NULL, NULL, 1, NULL);
